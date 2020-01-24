83eeff652a86659491f639ceb9572aac2be3930f
AnthoniaS <anthoniasr@gmail.com>
2020-01-24 18:33:38 -0300

    V.6

--- blog/resources/views/admin/artigos/index.blade.php
+++ blog/resources/views/admin/artigos/index.blade.php
@@ -6,6 +6,8 @@
      <tabela-lista 
      v-bind:titulos="['#','Título','Descrição']"
      v-bind:itens="[[1,'PHP OO','Curso de PHP OO'],[2,'Vue JS','Curso de Vue JS'],]"
+
+     ordem="desc" ordemCol="1"
      criar="#criar" detalhe="#detalhe" editar="#editar" deletar="#deletar" token="5565656"
 
 
