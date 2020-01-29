8e7d0ee4493c6193f232044a4e7ee5614ab002a4
AnthoniaS <anthoniasr@gmail.com>
2020-01-29 17:03:48 -0300

    V.9

--- blog/resources/assets/js/components/TabelaLista.vue
+++ blog/resources/assets/js/components/TabelaLista.vue
@@ -2,14 +2,13 @@
     <div>
            
         
-        <div class="form-inline">
-            <a v-if="criar && !modal" v-bind:href="criar">Criar</a>
-             <modallink v-if="criar && modal" nome="adicionar" titulo="Criar" css=""></modallink>
-        <div class="form-group pull-right">
-            <input type="search" class="form-control" placeholder="Buscar" v-model="buscar">
-
-        </div>    
-        </div>
+          <div class="form-inline">
+      <a v-if="criar && !modal" v-bind:href="criar">Criar</a>
+      <modallink v-if="criar && modal" tipo="link" nome="adicionar" titulo="Criar" css=""></modallink>
+      <div class="form-group pull-right">
+        <input type="search" class="form-control" placeholder="Buscar" v-model="buscar" >
+      </div>
+    </div>
 
 
         <table class="table table-striped table-hover">
@@ -22,115 +21,106 @@
          </thead>
 
         <tbody>
-         <tr v-for="(item,index) in lista">
-            <td v-for="i in item">{{i}}</td>
-            
+        <tr v-for="(item,index) in lista">
+          <td v-for="i in item">{{i}}</td>
 
-             <td v-if="detalhe || editar || deletar ">
-                <form v-bind:id="index" v-if="deletar && token" v-bind:action="index.html" method="post">
-                    <input type="hidden" name="method" value="DELETE">
-                    <input type="hidden" name="_token" v-bind:value="token">
-                    
+          <td v-if="detalhe || editar || deletar">
+            <form v-bind:id="index" v-if="deletar && token" v-bind:action="deletar" method="post">
+              <input type="hidden" name="_method" value="DELETE">
+              <input type="hidden" name="_token" v-bind:value="token">
 
-                   <a v-if="detalhe"  v-bind:href="detalhe">Detalhe |</a> 
-                 <a v-if="editar && modal"  v-bind:href="editar">Editar |</a> 
-                 <modallink v-if="editar && modal" nome="editar" titulo="Editar |" css=""></modallink>
+              <a v-if="detalhe" v-bind:href="detalhe">Detalhe |</a>
+              <a v-if="editar && !modal" v-bind:href="editar"> Editar |</a>
+              <modallink v-if="editar && modal" v-bind:item="item" tipo="link" nome="editar" titulo=" Editar |" css=""></modallink>
 
-                <a href="#" v-on:click="executaForm(index)">Deletar</a>
+              <a href="#" v-on:click="executaForm(index)"> Deletar</a>
 
-
-                </form>
-                <span v-if="!token">
-                <a v-if="detalhe"  v-bind:href="detalhe">Detalhe |</a> 
-                 <a v-if="editar && modal"  v-bind:href="editar">Editar |</a> 
-                 <modallink v-if="editar && modal" nome="editar" titulo="Editar |" css=""></modallink>
-                <a v-if="deletar"  v-bind:href="deletar">Deletar</a> 
-
-
-                </span>
-
-                <span v-if="token && !deletar">
-                <a v-if="detalhe"  v-bind:href="detalhe">Detalhe |</a> 
-                <a v-if="editar && !modal"  v-bind:href="editar">Editar |</a> 
-                 <modallink v-if="editar && modal" nome="editar" titulo="Editar |" css=""></modallink>
+            </form>
+            <span v-if="!token">
+              <a v-if="detalhe" v-bind:href="detalhe">Detalhe |</a>
+              <a v-if="editar && !modal" v-bind:href="editar"> Editar |</a>
+              <modallink v-if="editar && modal" tipo="link" nome="editar" titulo=" Editar |" css=""></modallink>
+              <a v-if="deletar" v-bind:href="deletar"> Deletar</a>
+            </span>
+            <span v-if="token && !deletar">
+              <a v-if="detalhe" v-bind:href="detalhe">Detalhe |</a>
+              <a v-if="editar && !modal" v-bind:href="editar"> Editar</a>
+              <modallink v-if="editar && modal" tipo="link" nome="editar" titulo=" Editar" css=""></modallink>
+            </span>
 
 
-                </span>
-                
-             </td>
+          </td>
+        </tr>
 
-         </tr>
-          
 
-        </tbody>
-     </table>
-    </div>
+      </tbody>
 
-   
+    </table>
+
+  </div>
+
 </template>
 
 <script>
-   export default{
-    props:['titulos','itens','ordem','ordemCol','criar','detalhe','editar','deletar','token','modal'],
-    data: function(){
+    export default {
+      props:['titulos','itens','ordem','ordemcol','criar','detalhe','editar','deletar','token','modal'],
+      data: function(){
         return {
-            buscar:'',
-            ordemAux: this.ordem || "asc",
-            ordemAuxCol: this.ordemcol || 0
+          buscar:'',
+          ordemAux: this.ordem || "asc",
+          ordemAuxCol: this.ordemcol || 0
         }
-    },
-    methods:{
+      },
+      methods:{
         executaForm: function(index){
-            document.getElementById(index).submit();
+          document.getElementById(index).submit();
         },
         ordenaColuna: function(coluna){
-            this.ordemAuxCol = coluna;
-            if(this.ordemAux.toLowerCase() == "asc"){
-                this.ordemAux = 'desc';
-            }else{
-                this.ordemAux = 'asc';
-            }
+          this.ordemAuxCol = coluna;
+          if(this.ordemAux.toLowerCase() == "asc"){
+            this.ordemAux = 'desc';
+          }else{
+            this.ordemAux = 'asc';
+          }
         }
-
-    },
-    computed:{
+      },
+      computed:{
         lista:function(){
 
-            let ordem = this.ordemAux;
-            let ordemCol = this.ordemAuxCol;
-            ordem = ordem.toLowerCase();
-            ordemCol = parseInt(ordemCol);
+          let ordem = this.ordemAux;
+          let ordemCol = this.ordemAuxCol;
+          ordem = ordem.toLowerCase();
+          ordemCol = parseInt(ordemCol);
 
-            if(ordem == "asc"){
-                this.itens.sort(function(a,b){
-                if( Object.values(a)[ordemCol] > Object.values(b)[ordemCol] ) {return 1;} 
-                if( Object.values(a)[ordemCol] < Object.values(b)[ordemCol] ) {return -1;} 
-                return 0;
+          if(ordem == "asc"){
+            this.itens.sort(function(a,b){
+              if (Object.values(a)[ordemCol] > Object.values(b)[ordemCol] ) { return 1;}
+              if (Object.values(a)[ordemCol] < Object.values(b)[ordemCol] ) { return -1;}
+              return 0;
             });
-
-            }else{
-                this.itens.sort(function(a,b){
-                if( Object.values(a)[ordemCol] < Object.values(b)[ordemCol]){return 1;} 
-                if( Object.values(a)[ordemCol] > Object.values(b)[ordemCol]){return -1;} 
-                return 0;
+          }else{
+            this.itens.sort(function(a,b){
+              if (Object.values(a)[ordemCol] < Object.values(b)[ordemCol] ) { return 1;}
+              if (Object.values(a)[ordemCol] > Object.values(b)[ordemCol] ) { return -1;}
+              return 0;
             });
-            }
+          }
 
-
-            if(this.buscar){
-                 return this.itens.filter(res => {
-                for(let k = 0;k <res.length; k++){
-                     if((res[k] + "").toLowerCase().indexOf(this.buscar.toLowerCase()) >= 0){
-                    return true;
+          if(this.buscar){
+            return this.itens.filter(res => {
+              for(let k = 0;k < res.length; k++){
+                if((res[k] + "").toLowerCase().indexOf(this.buscar.toLowerCase()) >= 0){
+                  return true;
                 }
-            }
-               return false;
+              }
+              return false;
+
             });
-            }
+          }
 
 
-            return this.itens;
+          return this.itens;
         }
+      }
     }
-   }
 </script>
--- blog/resources/views/admin/artigos/index.blade.php
+++ blog/resources/views/admin/artigos/index.blade.php
@@ -1,90 +1,55 @@
 @extends('layouts.app')
 
 @section('content')
-<pagina tamanho="10">
-  <painel titulo="Lista de Artigos">
-    <migalhas v-bind:lista="{{$listaMigalhas}}"> </migalhas>
+ <pagina tamanho="12">
+    <painel titulo="Lista de Artigos">
+      <migalhas v-bind:lista="{{$listaMigalhas}}"></migalhas>
 
 
-  
 
-   
-     <tabela-lista 
-     v-bind:titulos="['#','Título','Descrição']"
-     v-bind:itens="{{$listaArtigos}}"
+      <tabela-lista
+      v-bind:titulos="['#','Título','Descrição']"
+      v-bind:itens="{{$listaArtigos}}"
+      ordem="desc" ordemcol="1"
+      criar="#criar" detalhe="#detalhe" editar="#editar" deletar="#deletar" token="7887522323"
+      modal="sim"
 
-     ordem="desc" ordemCol="1"
-     criar="#criar" detalhe="#detalhe" editar="#editar" deletar="#deletar" token="5565656"
-     modal="sim"
+      ></tabela-lista>
 
-     ></tabela-lista>            
-     
+    </painel>
 
+  </pagina>
 
-</painel>
-</pagina>
-<modal nome="adicionar">
+  <modal nome="adicionar">
     <painel titulo="Adicionar">
-      <painel titulo="Adicionar">
-    <formulario css="" action="#" method="put" enctype="multipart/form=data" token="12345" >
-        <div class="form-group">
-            <label for="titulo">Título</label>
-            <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Título">    
+      <formulario css="" action="#" method="put" enctype="multipart/form-data" token="12345">
 
+        <div class="form-group">
+          <label for="titulo">Título</label>
+          <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Título">
         </div>
         <div class="form-group">
-            <label for="descricao">Descrição</label>
-            <input type="text" class="form-control" id="descricao" name="descricao" placeholder="Descrição">    
-
+          <label for="descricao">Descrição</label>
+          <input type="text" class="form-control" id="descricao" name="descricao" placeholder="Descrição">
         </div>
-        <button class="btn btn-info">Adicionar </button>
-        
-    </formulario>
+        <button class="btn btn-info">Adicionar</button>
+      </formulario>
+    </painel>
+  </modal>
+  <modal nome="editar">
+    <painel titulo="Editar">
+      <formulario css="" action="#" method="put" enctype="multipart/form-data" token="12345">
 
-
-</painel>
-</modal>
-
-<modal nome="editar">
-    <painel titulo="Adicionar">
-      <painel titulo="Adicionar">
-    <formulario css="" action="#" method="put" enctype="multipart/form=data" token="12345" >
         <div class="form-group">
-            <label for="titulo">Título</label>
-            <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Título">    
-
+          <label for="titulo">Título</label>
+          <input type="text" class="form-control" id="titulo" name="titulo" v-model="$store.state.item.titulo" placeholder="Título">
         </div>
         <div class="form-group">
-            <label for="descricao">Descrição</label>
-            <input type="text" class="form-control" id="descricao" name="descricao" placeholder="Descrição">    
-
+          <label for="descricao">Descrição</label>
+          <input type="text" class="form-control" id="descricao" name="descricao" v-model="$store.state.item.descricao" placeholder="Descrição">
         </div>
-        <button class="btn btn-info">Adicionar </button>
-        
-    </formulario>
-
-
-</painel>
-</modal>
-<modal nome="detalhe">
-    <painel titulo="Adicionar">
-      <painel titulo="Adicionar">
-    <formulario css="" action="#" method="put" enctype="multipart/form=data" token="12345" >
-        <div class="form-group">
-            <label for="titulo">Título</label>
-            <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Título">    
-
-        </div>
-        <div class="form-group">
-            <label for="descricao">Descrição</label>
-            <input type="text" class="form-control" id="descricao" name="descricao" placeholder="Descrição">    
-
-        </div>
-        <button class="btn btn-info">Adicionar </button>
-        
-    </formulario>
-
-
-</painel>
-</modal>
-    @endsection
+        <button class="btn btn-info">Atualizar</button>
+      </formulario>
+    </painel>
+  </modal>
+@endsection
